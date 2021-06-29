9deb102a96c325d0f341ebaa7bab41fdcda78d5c
rafael fernando <Rafael_FOSS@hotmail.com>
2021-06-29 14:10:24 -0300

    Primeiro Commit - Teste Tria Software

---
+++ Teste_Tria_Software/Teste_Tria_Software/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Teste_Tria_Software.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
